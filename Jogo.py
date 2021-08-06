45935ff3ddcf568c8fbffb8bec792ff10a3777bd
juliacfigueiredo <julia.figueiredo2003@icloud.com>
2021-08-05 21:48:35 -0300

    jogo inicial

---
+++ Jogo.py
@@ -0,0 +1,37 @@
+# ===== Inicialização =====
+# ----- Importa e inicia pacotes
+import pygame
+import random
+from config import WIDTH, HEIGHT, INIT, GAME, QUIT, GAME_OVER
+from init_screen import init_screen
+from game_screen import game_screen
+from game_over import game_over_screen
+
+
+pygame.init()
+pygame.mixer.init()
+
+# ----- Gera tela principal
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Navinha')
+
+state = INIT
+while state != QUIT:
+    if state == INIT:
+        state = init_screen(window)
+    elif state == GAME:
+        state = game_screen(window)
+    elif state == GAME_OVER:
+        state = game_over_screen(window)
+    else:
+        state = QUIT
+
+# ----- Gera tela 'game over'
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Navinha')
+
+
+
+# ===== Finalização =====
+pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
+
